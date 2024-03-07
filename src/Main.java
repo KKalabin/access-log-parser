00d1fc26a0710a7d22a0f0675f8694158ee9d4c7
Konstantin Kalabin <KSKalabintv@gmail.com>
2024-03-07 23:09:26 +0300

    Циклы. Каунтер корректных путей до файла

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text=new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " +text.length());
+        int countPositivePath = 0;
+        while (true) {
+            System.out.print("Укажите путь до файла: ");
+            String path = new Scanner(System.in).nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists || isDirectory) {
+                System.out.println("Указанный файл не существует или путь не является путем к файлу"); continue;
+            } else if (fileExists || isDirectory) {
+                countPositivePath++;
+                System.out.println("Путь указан верно");
+            }
+
+            System.out.println("Это файл номер "+ countPositivePath);
+        }
+        }
     }
-}
